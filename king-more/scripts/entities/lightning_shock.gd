36f1327a761376f6a426981ec981fe833177dc74
Ryan Don <rjdon9@gmail.com>
2026-03-31 18:23:44 -0400

    Removed animation from lightning wep

--- king-more/scripts/entities/lightning_shock.gd
+++ king-more/scripts/entities/lightning_shock.gd
@@ -12,6 +12,7 @@ extends Node2D
 @onready var b_sprite = $point_b/AnimatedSprite2D
 
 var target_pos: Vector2 = Vector2.ZERO
+var opacity: float = 100.0
 
 func _ready() -> void:
 	var distance = position.distance_to(target_pos)
@@ -40,6 +41,11 @@ func _ready() -> void:
 	_update_segment(start_pos, a_pos, start_sprite)
 	_update_segment(a_pos, b_pos, a_sprite)
 	_update_segment(b_pos, end_pos, b_sprite)
+
+func _process(delta: float) -> void:
+	print(opacity)
+	opacity -= 5.0
+	modulate.a = opacity
 
 func _update_segment(a: Vector2, b: Vector2, sprite: AnimatedSprite2D) -> void:
 	var dir := b - a
